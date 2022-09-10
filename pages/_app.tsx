db922f7bbd263f5d76eeb72ab6fd4350aa562af2
Matheus Nascimento <matheus.n16@bkpi.io>
2022-09-09 20:04:08 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals'
+import type {AppProps} from 'next/app'
+import {ThemeProvider} from 'styled-components'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import theme from '../styles/theme'
+import GlobalStyles from '../styles/globals'
+
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
